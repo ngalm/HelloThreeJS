0ca7a78ad84449717cab5dc17289ecc20fc4168c
natalia alm <nataliaalm@natalias-MacBook-Air.local>
2026-02-17 10:33:33 -0800

    deleted FP camera control imp

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
 
+import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
+
+
 // set up loader, scene, camera, and renderer
 const loader = new GLTFLoader();
 const scene = new THREE.Scene();
@@ -11,13 +14,9 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-// First person camera controls
-const clock = new THREE.Clock();  // set up a clock to track changes in keyboard/mouse input
-const controls = new FirstPersonControls(camera, renderer.domElement);
-controls.movementSpeed = 5;
-controls.lookSpeed = .1;  
-controls.autoForward = false;     // camera does NOT automatically move forward        
-const DEAD_ZONE = 100;            // cursor dead zone size where no camera rotation occurs
+// Pointer Lock camera control
+const controls = new PointerLockControls( camera, document.body );
+// LOOK AT DOCUMENTATION EX COED
 
 // load landscape model
 loader.load( './assets/helloWorld.glb', function ( gltf ) {
@@ -42,32 +41,8 @@ scene.add(sunlight);
 const ambient = new THREE.AmbientLight(0x4040ff, 1);
 scene.add(ambient);
 
-/** 
- * Determines whether mouse is in the dead zone. 
- * If it is, sets mouse coordinates to 0 so that no camera rotation occurs since no angle change.
- * @param {object} controls A FirstPersonControl object. 
- * @param {number} radius The radius of the dead zone.
- *              ! Note ! 
- *        This function depends on FirstPersonControl's implementation of camera rotation 
- *        and its naming of pointerX and pointerY properties. Idk if that'll change in the
- *        future and break this func.
- */
-function applyDeadZone(controls, radius) {
-  const x = controls.pointerX;
-  const y = controls.pointerY;
-  const dist = Math.sqrt(x * x + y * y);
-
-  if (dist < radius) {
-    controls.pointerX = 0;
-    controls.pointerY = 0;
-  }
-}
-
 // animate scene
 function animate() {
-  const delta = clock.getDelta();       // time since last frame
-  applyDeadZone(controls, DEAD_ZONE);
-  controls.update(delta);               // process keyboard/mouse movement
   renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
